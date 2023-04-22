121f4677fb30ebc2209f7f6a0e5e4bb7a211a815
Alejandro Saez <alejandrosaezc@gmail.com>
2023-04-22 17:10:38 +0200

    feat(Pagination.tsx, HomePage.tsx): create and add pagination component

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+
+interface Props {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+export const Pagination: React.FC<Props> = ({
+  currentPage,
+  totalPages,
+  onPageChange,
+}) => {
+  const [pagesToShow, setPagesToShow] = useState(5);
+  const [currentPageIndex, setCurrentPageIndex] = useState(currentPage - 1);
+
+  const handleClick = (index: number) => {
+    setCurrentPageIndex(index);
+    onPageChange(index + 1);
+  };
+
+  const handlePrevClick = () => {
+    if (currentPageIndex > 0) {
+      setCurrentPageIndex(currentPageIndex - 1);
+      onPageChange(currentPageIndex);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (currentPageIndex < totalPages - 1) {
+      setCurrentPageIndex(currentPageIndex + 1);
+      onPageChange(currentPageIndex + 2);
+    }
+  };
+
+  const renderPages = () => {
+    const pages = [];
+
+    for (let i = 0; i < totalPages; i++) {
+      if (
+        i === 0 ||
+        i === totalPages - 1 ||
+        (i >= currentPageIndex - pagesToShow &&
+          i <= currentPageIndex + pagesToShow)
+      ) {
+        pages.push(
+          <button
+            key={i}
+            onClick={() => handleClick(i)}
+            style={{
+              margin: "0.25rem",
+              fontWeight: i === currentPageIndex ? "bold" : "normal",
+            }}
+          >
+            {i + 1}
+          </button>
+        );
+      } else if (pages[pages.length - 1].props.children !== "...") {
+        pages.push(
+          <span key={i} style={{ margin: "0.25rem" }}>
+            ...
+          </span>
+        );
+      }
+    }
+
+    return pages;
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <button onClick={handlePrevClick} disabled={currentPageIndex === 0}>
+        {"<"}
+      </button>
+      {renderPages()}
+      <button
+        onClick={handleNextClick}
+        disabled={currentPageIndex === totalPages - 1}
+      >
+        {">"}
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,17 +2,26 @@ import React, { useEffect, useState } from "react";
 import { searchGiphy } from "../services/giphyService";
 import useDebounce from "../hooks/useDebounce";
 import GifGrid from "../components/gifGrid";
+import Pagination from "../components/Pagination";
 
 export const HomePage = () => {
   const [searchValue, setSearchValue] = useState<string>("");
   const debouncedSearchValue = useDebounce(searchValue, 300);
   const [gifs, setGifs] = useState<[]>([]);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [offset, setOffset] = useState<number>(0);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(18);
+
+  const handlePageChange = (page: number) => {
+    setOffset((page - 1) * rowsPerPage);
+  };
   useEffect(() => {
-    searchGiphy(debouncedSearchValue, 20, 5).then((gifs) => {
+    setGifs([]);
+    searchGiphy(debouncedSearchValue, offset, rowsPerPage).then((gifs) => {
       setGifs(gifs.data.data);
-      console.log(gifs.data.data);
+      setTotalPages(gifs.data.pagination.total_count / rowsPerPage);
     });
-  }, [debouncedSearchValue]);
+  }, [debouncedSearchValue, offset, rowsPerPage]);
   return (
     <div>
       <h1>Hello World!</h1>
@@ -22,6 +31,11 @@ export const HomePage = () => {
         onChange={(e) => setSearchValue(e.target.value)}
       />
       <GifGrid gifs={gifs} />
+      <Pagination
+        totalPages={totalPages}
+        currentPage={offset / rowsPerPage + 1}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 };
