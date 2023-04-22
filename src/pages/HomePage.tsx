0d296eec5ac954e98a552247c9e16d86ac276366
Alejandro Saez <alejandrosaezc@gmail.com>
2023-04-22 18:27:41 +0200

    style(HomePage.tsx, HomePage.css): add grid layout and styling to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import { searchGiphy } from "../services/giphyService";
 import useDebounce from "../hooks/useDebounce";
 import GifGrid from "../components/gifGrid";
 import Pagination from "../components/Pagination";
+import "../styles/homePage.css";
 
 export const HomePage = () => {
   const [searchValue, setSearchValue] = useState<string>("");
@@ -10,7 +11,7 @@ export const HomePage = () => {
   const [gifs, setGifs] = useState<[]>([]);
   const [totalPages, setTotalPages] = useState<number>(1);
   const [offset, setOffset] = useState<number>(0);
-  const [rowsPerPage, setRowsPerPage] = useState<number>(18);
+  const [rowsPerPage, setRowsPerPage] = useState<number>(12);
 
   const handlePageChange = (page: number) => {
     setOffset((page - 1) * rowsPerPage);
@@ -23,19 +24,24 @@ export const HomePage = () => {
     });
   }, [debouncedSearchValue, offset, rowsPerPage]);
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <input
-        type="text"
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
-      />
+    <div className="container">
+      <div className="top-container">
+        <div className="header">Search for your favorite Gif</div>
+        <input
+          className="search-input"
+          type="text"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </div>
       <GifGrid gifs={gifs} />
-      <Pagination
-        totalPages={totalPages}
-        currentPage={offset / rowsPerPage + 1}
-        onPageChange={handlePageChange}
-      />
+      <div className="bottom-container">
+        <Pagination
+          totalPages={totalPages}
+          currentPage={offset / rowsPerPage + 1}
+          onPageChange={handlePageChange}
+        />
+      </div>
     </div>
   );
 };
